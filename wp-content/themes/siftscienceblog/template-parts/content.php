986126f36c2c226adc2ec23da10afd33d3f60715
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-04-15 16:31:49 -0400

    Add image and fix article layout

--- wp-content/themes/siftscienceblog/template-parts/content.php
+++ wp-content/themes/siftscienceblog/template-parts/content.php
@@ -10,33 +10,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<?php
-			if ( is_single() ) {
-				  the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
+    <div class="left">
+    		<?php
+					if ( is_single() ) {
+						  the_title( '<h1 class="entry-title">', '</h1>' );
+					} else {
+						the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+					}
 
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<span><?php the_date('F d, Y', '<strong>', '</strong>'); ?> by <?php the_author(); ?></span>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-
-
-	<div class="entry-content">
-		<div class="post-excerpt">
-			<p><?php the_excerpt(); ?></p>
-		</div>
-		<div class="featured-image">
+			if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<span><?php the_date('F d, Y', '<strong>', '</strong>'); ?> by <?php the_author(); ?></span>
+			</div><!-- .entry-meta -->
 			<?php
+			endif; ?>
+			<p><?php the_excerpt(); ?></p>
+    </div>
+    <div class="right">
+        <?php
 				if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
 				  the_post_thumbnail();
 				}
 			?>
-		</div>
-	</div><!-- .entry-content -->
+    </div>
+</article>
 
-
-</article><!-- #post-## -->
