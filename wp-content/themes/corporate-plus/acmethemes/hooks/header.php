a841f7c72e58a72353b679e20a29078433a73189
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-04-19 23:00:00 -0400

    Add bootstrap fixed nav

--- wp-content/themes/corporate-plus/acmethemes/hooks/header.php
+++ wp-content/themes/corporate-plus/acmethemes/hooks/header.php
@@ -142,59 +142,48 @@ if ( ! function_exists( 'corporate_plus_header' ) ) :
             $inner_nav .= ' navbar-no-fs';
         }
         ?>
-        <div class="navbar at-navbar navbar-fixed-top <?php echo esc_attr( $inner_nav );?>" id="navbar" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                        <svg class="veggieburger">
-                            <rect y="0" width="24" height="4"></rect>
-                            <rect y="8" width="24" height="4"></rect>
-                            <rect y="16" width="24" height="4"></rect>
-                        </svg>
-                    </button>
 
-                    <a href="/" class="sift-brand">
-                      <svg class="color-logo">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-color-logo"></use>
-                      </svg>
-                      <svg class="color-hexagon">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-color-hexagon"></use>
-                      </svg>
-                      <svg class="monochrome-logo">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-monochrome-logo"></use>
-                      </svg>
-                      <svg class="monochrome-hexagon">
-                        <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-monochrome-hexagon"></use>
-                      </svg>
-                    </a>
-
-                </div>
-                <div class="main-navigation navbar-collapse collapse">
-                    <?php
-                    if( is_front_page() && !is_home() && has_nav_menu( 'one-page') ){
-                        wp_nav_menu(
-                            array(
-                                'theme_location' => 'one-page',
-                                'menu_id' => 'primary-menu',
-                                'menu_class' => 'nav navbar-nav navbar-left animated',
-                            )
-                        );
-
-                    }
-                    else{
-                     wp_nav_menu(
-                        array(
-                            'theme_location' => 'primary',
-                            'menu_id' => 'primary-menu',
-                            'menu_class' => 'nav navbar-nav navbar-left animated',
-                        )
-                    );
-                    }
-                   ?>
-                </div>
-                <!--/.nav-collapse -->
-            </div>
+        <nav class="navbar navbar-default navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <svg class="veggieburger">
+            <rect y="0" width="24" height="4"></rect>
+            <rect y="8" width="24" height="4"></rect>
+            <rect y="16" width="24" height="4"></rect>
+          </svg>
+          </button>
+          <a class="navbar-brand" href="#">Sift Science</a>
         </div>
+        <div id="navbar" class="navbar-collapse collapse">
+          <ul class="nav navbar-nav">
+            main-nav
+            <!-- <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+                <li role="separator" class="divider"></li>
+                <li class="dropdown-header">Nav header</li>
+                <li><a href="#">Separated link</a></li>
+                <li><a href="#">One more separated link</a></li>
+              </ul> -->
+            </li>
+          </ul>
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="../navbar/">Default</a></li>
+            <li><a href="../navbar-static-top/">Static top</a></li>
+            <li class="active"><a href="./">Fixed top <span class="sr-only">(current)</span></a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+
         <?php
         if( 1 != $corporate_plus_enable_feature ) {
             echo "<div class='no-fs-clearfix'></div>";
