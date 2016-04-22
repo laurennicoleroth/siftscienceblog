3182bffa62e3fa06d8c1d9a128138d7141342a62
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-04-21 20:06:23 -0400

    Add functioning expanding search bar

--- wp-content/themes/corporate-plus/index.php
+++ wp-content/themes/corporate-plus/index.php
@@ -50,15 +50,17 @@ get_header(); ?>
   	</div>
   	<div class="col-md-4">
   		<!-- <?php get_search_form( ); ?> -->
-      <form role="search" method="get" class="search-form" action="http://prodigitalnomad.com/">
+
+      <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
         <label>
+          <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
           <input type="search"
                 class="search-field"
-                placeholder="Search"
-                value="" name="s"
-                title="Search efter:">
+                placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
+                value="<?php echo get_search_query() ?>" name="s"
+                title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">
         </label>
-        <input type="submit" class="search-submit" value="Search">
+        <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
       </form>
   		<!-- <svg class="icon-search">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-search"></use>
