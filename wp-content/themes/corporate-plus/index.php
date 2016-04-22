817e29bbac3ed9285315efa7fac7ece68cef8752
Lauren N. Roth <laurennicoleroth@gmail.com>
2016-04-22 12:48:29 -0400

    Add icon and make it pretty blue.

--- wp-content/themes/corporate-plus/index.php
+++ wp-content/themes/corporate-plus/index.php
@@ -54,13 +54,16 @@ get_header(); ?>
       <form role="search" method="get" class="search-form" action="<?php echo home_url( '/' ); ?>">
         <label>
           <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label' ) ?></span>
+          <i class="fa fa-search" aria-hidden="true"></i>
           <input type="search"
                 class="search-field"
                 placeholder="<?php echo esc_attr_x( 'Search …', 'placeholder' ) ?>"
                 value="<?php echo get_search_query() ?>" name="s"
                 title="<?php echo esc_attr_x( 'Search for:', 'label' ) ?>">
         </label>
-        <input type="submit" class="search-submit" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
+        <input type="submit" class="search-submit fa fa-search" value="<?php echo esc_attr_x( 'Search', 'submit button' ) ?>">
+
+
       </form>
   		<!-- <svg class="icon-search">
         <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-search"></use>
